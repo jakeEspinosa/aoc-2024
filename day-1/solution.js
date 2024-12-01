88b2db5277a3b8b09644150fb0536ba912dbc33c
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2024-12-01 10:39:54 -0700

    fully process input

--- day-1/solution.js
+++ day-1/solution.js
@@ -1,23 +1,50 @@
 /* 
-parse the input and split into two lists
 sort the two lists
 compare them, find the differences, and add to res
 return res
 */
-
 const fs = require("node:fs");
 const readline = require("node:readline");
 
-const readLineByLine = async () => {
-  const fileStream = fs.createReadStream("/home/jake/learning/aoc-2024/day-1/input.txt");
+const PATH = "/home/jake/learning/aoc-2024/day-1/input.txt";
+
+const splitLine = (line) => {
+  let firstNum = "";
+  let secondNum = "";
+  let isInSecondNum = false;
+
+  for (const c of line) {
+    if (c === " ") 
+      isInSecondNum = true;
+    else
+      if (isInSecondNum === false) 
+        firstNum += c;
+      else
+        secondNum += c;
+    }
+
+  return [parseInt(firstNum), parseInt(secondNum)];
+};
+
+const processInput = async (path) => {
+  const fileStream = fs.createReadStream(path);
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
+  const firstNums = [];
+  const secondNums = [];
 
   for await (const line of rl) {
-    console.log(line);
+    const [first, second] = splitLine(line);
+    firstNums.push(first);
+    secondNums.push(second);
   }
+  return [firstNums, secondNums];
 };
 
-readLineByLine();
+const main = async () => {
+  const [firstNums, secondNums] = await processInput(PATH);
+};
+
+main();
